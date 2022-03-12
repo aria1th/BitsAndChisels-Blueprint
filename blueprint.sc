bb10c4d01ea37f2bb0022b1f200d40deb62b4e76
AngelBottomless <35677394+aria1th@users.noreply.github.com>
2022-03-12 23:51:58 +0900

    Add basic conversion scripts

---
+++ blueprint.sc
@@ -0,0 +1,115 @@
+__config()->{
+	'stay_loaded' : true,
+	'scope'->'global',
+	'commands' -> {
+		' ' -> '__print_default',
+		'info' -> '_palette',
+		'save <name>' ->'saveAs',
+		'load <savedblueprint>' -> 'loadAs'
+		},
+	'arguments' -> {
+		'savedblueprint' -> {'type' -> 'term',
+			 'suggester'->_(args) -> (
+                        list_files('','nbt')
+      				 ),
+			},
+		'name' -> {'type' -> 'term'},
+		}
+};
+
+global_save_limit = 50;
+
+__print_default()->(
+	print(player(), '/blueprint info -> prints required materials');
+	print(player(), '/blueprint save <name> -> saves current blueprint data as <name>, OP only');
+	print(player(), '/blueprint load <name> -> loads blueprint data into current blueprint, overrides data' );
+);
+
+_is_empty()->(
+	player()~'holds':0 == 'bitsandchisels:blueprint' && player()~'holds':2 == null
+);
+_is_not_empty()->(
+	player()~'holds':0 == 'bitsandchisels:blueprint' && player()~'holds':2 != null
+);
+_holds()->(
+	player()~'holds':0 == 'bitsandchisels:blueprint'
+);
+_palette()->(
+	if(_is_empty(), print(player(), 'No blueprint is being holded'); return());
+	palette = parse_nbt(player()~'holds':2:'blueprint.palette[]'); //lists of nbts
+	bits = player()~'holds':2:'blueprint.bits_v2[]';
+	hist = {};
+	if(bits == null, return(null));
+	i = 0;
+	for(range(4096),
+		hist : ((bits : (2*_+1))*256 + (bits : (2*_))) += 1
+	);
+	for(hist,
+		name = palette:_:'Name';
+		count = hist:_;
+		print(player(), 'Bits : '+name+ ' '+count);
+	)
+);
+getTime()->(
+	t = convert_date(unix_time());
+	''+ t:0 + t:1 + t:2
+);
+
+_check_save_limit()->(
+	if (player()~'permission_level'>= 3, return(true));
+	for(list_files('','nbt'),
+		_~(player()~'name')
+	) < global_save_limit
+);
+
+saveAs(fileName)->(
+	//if(player()~'permission_level'<3, print(player(), 'Not enough permission level : ask for gm');return());
+	if(!_check_save_limit(), print('Too many saved blueprints : ask for gm');return());
+	if(_is_not_empty(),
+		data = player()~'holds':2;
+		write_file((player()~'name')+getTime()+fileName, 'nbt', data);
+	);
+	print(player(), 'Saved blueprint as '+fileName)
+);
+
+loadAs(fileName)->(
+	if(!_holds(),  print(player(), 'No blueprint is being holded');return());
+	file = read_file(fileName, 'nbt');
+	if(file == null, print(player(), 'No file exists'); return());
+	slot = player()~'selected_slot';
+	inventory_set(player(), slot, 1, 'bitsandchisels:blueprint', file);
+	print(player(), 'Successfully loaded blueprint')
+);
+
+loadRawAndSet(fileName, pos)->(
+	file = read_file(fileName, 'nbt');
+	nbt = file:'blueprint';
+	set(pos, 'bitsandchisels:bits_block', ['waterlogged', 'false'], nbt)
+);
+
+getFilesContains(path, delimeter)->(
+	lf = list_files(path, 'nbt');
+	ll = [];
+	for(lf, if(_~delimeter,put(ll, null, _) ));
+	ll
+);
+
+processString(string, y)->(
+	a = split('/',string);
+	a = get(a, length(a)-1);
+	a =replace(a, 'a', '_');
+	a =replace(a, 'b', '_');
+	s = split('_',a);
+	print(s)
+	[number(get(s,1)), y, number(get(s,3))]
+);
+
+pasteFrom(path, delimeter, origin,y)->(
+	for(getFilesContains(path, delimeter),
+		fileName = _;
+		posO = processString(_,y) + origin;
+		loadRawAndSet(fileName, posO);	
+		sleep(50);
+	);
+	
+)
---
+++ convert.py
@@ -0,0 +1,120 @@
+import nbtlib
+import numpy as np
+import re
+
+def open_file(name):
+    return nbtlib.load(name)
+#returns X Y Z
+
+def get_dimension(file):
+    return map(int, [file['Width'], file['Height'], file['Length']])
+
+def getRequiredSize(file):
+    X,Y,Z = get_dimension(file)
+    return np.ceil(np.array([X,Y,Z]) / 16)
+def prepareArray(file):
+    X,Y,Z = getRequiredSize(file)
+    return np.zeros(X*Y*Z, dtype = np.int16)
+
+def getByteArray(file):
+    return file['BlockData']
+def remap(palette):
+    retVal = dict()
+    for keys in palette:
+        retVal[palette[keys]] = genCompound(keys)
+    return retVal
+
+def genCompound(fromString):
+    prop = re.findall('\[(.*?]*)\]',fromString)
+    propComp = dict()
+    for props in prop:
+        key, val = props.split('=')
+        propComp[key] = nbtlib.String(val)
+    propCompound = nbtlib.Compound({'Name' : nbtlib.String(re.sub('\[(.*?]*)\]','',fromString))})
+    if propComp:
+        propCompound['Properties']= nbtlib.Compound(propComp)
+    return propCompound
+
+    
+class Schem:
+    def __init__(self, file):
+        x,y,z = get_dimension(file)
+        self.sizeX =x
+        self.sizeY =y
+        self.sizeZ =z
+        self.sizeLayer =x*z
+        self.byteLen = x*y*z
+        self.byteArray = getByteArray(file)
+        self.npArray = np.array(self.byteArray).reshape(y,z,x)
+        self.palette = remap(file['Palette'])
+        self.paletteSize = file['PaletteMax']
+        self.palette[self.paletteSize] = nbtlib.Compound({
+                                    'Name' : nbtlib.String('minecraft:air')})
+    def getIndex(self, x,y,z):
+        return y*self.sizeLayer + z*self.sizeX + x
+    def isOutofBound(self, x,y,z):
+        return x >= self.sizeX or y >= self.sizeY or z >= self.sizeZ
+    def getRequiredSize(self):
+        return map(int, np.ceil(np.array([self.sizeX, self.sizeY, self.sizeZ]) / 16))
+    def getInt(self,x,y,z):
+        if self.isOutofBound(x,y,z):
+            return self.paletteSize
+        return self.byteArray[self.getIndex(x,y,z)]
+    def translateBlock(self, integer : int):
+        return self.palette[integer]
+    def getBlock(self,x,y,z):
+        return self.translateBlock(self.getInt(x,y,z))
+    def getSection(self, x, y, z):
+        ret = np.ones((16,16,16),self.paletteSize, dtype=np.int16) 
+        for xi in range(16):
+            for yi in range(16):
+                for zi in range(16):
+                    ret[xi,yi,zi] = self.getInt(xi+x*16, yi+y*16, zi+z*16)
+        return ret
+    def getFlatSection(self, x, y, z):
+        ret = np.full(4096,self.paletteSize, dtype = np.int16)
+        _ = 0
+        for xi in range(16):
+            for yi in range(16):
+                for zi in range(16):
+                    ret[_] = self.getInt(xi+x*16, yi+y*16, zi+z*16)
+                    _+=1
+        return ret
+    def getArrayConverted(self, x,y,z):
+        section = self.getFlatSection(x,y,z)
+        unique = np.unique(section)
+        nbtlist = []
+        translatemap = dict()
+        for idx, val in enumerate(unique):
+            nbtlist.append(self.palette[val])
+            translatemap[val] = idx
+        return Schem.getSafeV2Array(Schem.translate(section, translatemap)), nbtlib.List[nbtlib.Compound](nbtlist)
+    def getSafeV2Array(v1arr):
+        ret = np.zeros(8192, np.int8)
+        for i in range(4096):
+            ret[2*i] = (v1arr[i] & 0xff)
+            ret[2*i+1] = ((v1arr[i]>>8) & 0xff)
+        return ret
+    def getFileToSave(self,x,y,z):
+        arr, paletteList = self.getArrayConverted(x,y,z)
+        innercomp = nbtlib.Compound({'bits_v2' : nbtlib.ByteArray(arr), 'palette' : paletteList})
+        outerComp = nbtlib.File({'blueprint': innercomp})
+        return outerComp
+    def saveAs(self, x,y,z, fileName, compressed = True):
+        w = self.getFileToSave(x,y,z)
+        w.save(fileName+'.nbt', gzipped=compressed)
+    def translate(A,dictionary):
+        sort_idx = np.argsort(list(dictionary.keys()))
+        idx = np.searchsorted(list(dictionary.keys()), A, sorter = sort_idx)
+        return np.asarray(list(dictionary.values()))[sort_idx][idx]
+    def saveAll(self, defaultName = 'a'):
+        X,Y,Z = self.getRequiredSize()
+        for x in range(X):
+            for y in range(Y):
+                for z in range(Z):
+                    self.saveAs(x,y,z,defaultName+'b'.join(map(str, [x,y,z])))
+        return X*Y*Z
+        
+#a = Schem(nbtlib.load('mumeibig.schem'))
+#a.saveAll('birb')
+    
